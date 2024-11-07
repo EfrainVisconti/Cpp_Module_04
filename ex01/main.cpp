ffc2a91e7e46d65b9cd6789a71918e7477b3fd4c
EV <144735628+EfrainVisconti@users.noreply.github.com>
2024-11-07 13:56:30 +0000

    adding deep copies test to main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 21:35:07 by codespace         #+#    #+#             */
-/*   Updated: 2024/11/06 17:48:04 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/07 13:53:15 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,52 @@
 #include "WrongCat.hpp"
 
 #define SIZE 10
+#define INDEX 0
 const std::string RESET = "\033[0m";
 const std::string GREEN = "\033[32m";
 
 int main()
 {
-    const Animal *animals[SIZE];
-    int index = 0;
-
-    std::cout << std::endl << GREEN << "-----Creating 5 Cats and 5 Dogs-----" << RESET << std::endl;
-    for (int i = 0; i < SIZE; i++)
     {
-        std::cout << GREEN << i << RESET << std::endl;
-        if (i % 2 == 0)
-            animals[i] = new Cat();
-        else
-            animals[i] = new Dog();
-    }
+        const Animal *animals[SIZE];
 
-    std::cout << std::endl << GREEN << "-----Checking Cats objects-----" << RESET << std::endl;
-    for (int i = 0; i < SIZE - 1; i += 2)
-    {
-        std::cout << GREEN << i << RESET << std::endl;
-        animals[i]->makeSound();
-        std::cout << animals[i]->getType() << std::endl;
-        std::cout << dynamic_cast<const Cat*>(animals[i])->getBrainIdeas(index) << std::endl;
-        dynamic_cast<const Cat*>(animals[i])->setBrainIdeas("diff_idea", index);
-        std::cout << dynamic_cast<const Cat*>(animals[i])->getBrainIdeas(index) << std::endl;
-    }
+        std::cout << std::endl << GREEN << "-----Creating 5 Cats and 5 Dogs-----" << RESET << std::endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            std::cout << GREEN << i << RESET << std::endl;
+            if (i % 2 == 0)
+                animals[i] = new Cat();
+            else
+                animals[i] = new Dog();
+        }
 
-    std::cout << std::endl << GREEN << "-----Deleting Cats and Dogs-----" << RESET << std::endl;
-    for (int i = 0; i < SIZE; i++)
-    {
-        std::cout << GREEN << i << RESET << std::endl;
-        delete animals[i];
+        std::cout << std::endl << GREEN << "-----Checking Cats objects-----" << RESET << std::endl;
+        for (int i = 0; i < SIZE - 1; i += 2)
+        {
+            std::cout << GREEN << i << RESET << std::endl;
+            animals[i]->makeSound();
+            std::cout << animals[i]->getType() << std::endl;
+            std::cout << dynamic_cast<const Cat*>(animals[i])->getBrainIdeas(INDEX) << std::endl;
+            dynamic_cast<const Cat*>(animals[i])->setBrainIdeas("diff_idea", INDEX);
+            std::cout << dynamic_cast<const Cat*>(animals[i])->getBrainIdeas(INDEX) << std::endl;
+        }
+
+        std::cout << std::endl << GREEN << "-----Deleting Cats and Dogs-----" << RESET << std::endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            std::cout << GREEN << i << RESET << std::endl;
+            delete animals[i];
+        }
     }
-    //TO DO: TEST DEEP COPY
+    {
+        std::cout << std::endl << GREEN << "-----Deep copies Test-----" << RESET << std::endl;
+        Dog a;
+        Dog b;
+        std::cout << "Dog 'a' idea 0 before change: " << a.getBrainIdeas(INDEX) << std::endl;
+        a.setBrainIdeas("test_idea", INDEX);
+        std::cout << "Dog 'a' idea 0 after change: " << a.getBrainIdeas(INDEX) << std::endl;
+        b = a;
+        std::cout << "Dog 'b' idea 0 to show deep copy : " << a.getBrainIdeas(INDEX) << std::endl;
+    }
     return 0;
 }
